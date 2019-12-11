233b08fc112a2ee9ae1b8097dcfbb9e2f544d45a
avtara <avtarakhrisna1@gmail.com>
2019-12-11 17:56:28 +0700

    Initial commit

---
+++ app/src/main/java/com/chutzpah/mylistview/Hero.java
@@ -0,0 +1,32 @@
+package com.chutzpah.mylistview;
+
+public class Hero {
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private int photo;
+    private String name;
+    private String description;
+
+}
